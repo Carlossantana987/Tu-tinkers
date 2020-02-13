a8c27528006724c810392e95733bc66bdced9784
Carlos <carlossantana@Natalie-Morriss-MacBook-Pro.local>
2020-02-12 23:21:12 -0500

    changing create-react-app boilerplate

---
+++ tutinkers/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+function App() {
+  return (
+    <div className = "container">
+      <div className = "row" >
+        <div className = "col-6">column number one </div>
+        <div className = "col-6">column number two </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
