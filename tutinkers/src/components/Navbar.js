d6db09dda0389f1ea2a27fa9bc20cb00be14cddc
Carlos <carlossantana@Natalie-Morriss-MacBook-Pro.local>
2020-02-15 11:12:58 -0500

    working on navbar

--- tutinkers/src/components/Navbar.js
+++ tutinkers/src/components/Navbar.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+import ribbon from '../img/bowLogo.png'
 
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello from Navbar</h3>
-            </div>
+            <nav className = "navbar navbar-expand-sm bg-teal navbar-dark px-sm-5">
+                
+                <Link to= '/'>
+                    <img src={ribbon} width="50px" alt="store" className ="navbar-brand"/> 
+                </Link>
+                
+                <ul className = "navbar-nav align-items-center">
+                    <li className = "nav-item ml-5">
+                        <Link to ="/" className= 'nav-link'>
+                            Products
+                        </Link>
+                    </li>
+                </ul>
+                
+                <Link to ="/cart" className= "ml-auto">
+                    <button>
+                        <i className="fas fa-cart-plus"/>
+                        My Cart
+                    </button>
+                </Link>
+            </nav>
         );
     }
 }
